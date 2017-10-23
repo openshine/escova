8b40f87361070e43cc27619bb07ab2e23907f336
Santiago Saavedra <ssaavedra@openshine.com>
2017-10-23 12:15:48 +0200

    Add all ES-related files to create the ES Plugin

---
+++ src/main/scala/com/openshine/escova/esplugin/EscovaComplexityAction.java
@@ -0,0 +1,69 @@
+package com.openshine.escova.esplugin;
+
+import com.openshine.escova.ComplexityMeasure;
+import com.openshine.escova.Parser;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.bytes.BytesArray;
+import org.elasticsearch.common.bytes.BytesReference;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.rest.*;
+import org.elasticsearch.rest.action.RestStatusToXContentListener;
+import org.elasticsearch.rest.action.cat.AbstractCatAction;
+import org.elasticsearch.rest.action.search.RestSearchAction;
+
+import java.io.IOException;
+
+import static org.elasticsearch.rest.RestRequest.Method.GET;
+import static org.elasticsearch.rest.RestRequest.Method.POST;
+import static org.elasticsearch.rest.action.search.RestSearchAction.parseSearchRequest;
+
+/**
+ * @author Santiago Saavedra (ssaavedra@openshine.com)
+ */
+public class EscovaComplexityAction extends BaseRestHandler {
+    public EscovaComplexityAction(Settings settings, RestController controller) {
+        super(settings);
+
+        controller.registerHandler(GET, "/_searchv", this);
+        controller.registerHandler(POST, "/_searchv", this);
+        controller.registerHandler(GET, "/{index}/_searchv", this);
+        controller.registerHandler(POST, "/{index}/_searchv", this);
+        controller.registerHandler(GET, "/{index}/{type}/_searchv", this);
+        controller.registerHandler(POST, "/{index}/{type}/_searchv", this);
+    }
+
+
+    @Override
+    protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+
+        request.withContentOrSourceParamParserOrNull(parser ->
+                parseSearchRequest(searchRequest, request, parser));
+
+        ComplexityMeasure<Object> analyze = Parser.analyze(searchRequest.source());
+
+        return channel -> {
+            RestResponse response = new RestResponse() {
+                @Override
+                public String contentType() {
+                    return "text/plain";
+                }
+
+                @Override
+                public BytesReference content() {
+                    return new BytesArray("The complexity is: " + analyze);
+                }
+
+                @Override
+                public RestStatus status() {
+                    return RestStatus.OK;
+                }
+            };
+            channel.sendResponse(response);
+        };
+        /*
+         return channel -> client.search(searchRequest, newRestStatusToXContentListener<>(channel));
+         */
+    }
+}
